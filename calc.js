efcd441a7ee24d53969f543e17378d23cf868f56
PatoGMTP <93350228+PatoGMTP@users.noreply.github.com>
2021-11-29 17:29:18 -0600

    Progress as of Monday 29th

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-var output = document.querySelector('.output');
+let output = document.querySelector('.output');
 /*declare the following variables and types
   num1 as number
   num2 as number
@@ -6,8 +6,19 @@ var output = document.querySelector('.output');
   
   Figure out how to set the values of num1 and num2 as the buttons are clicked
   */
-document.querySelector('.calc').addEventListener('click', function (evt) {
-    var button = evt.target;
+let operands_arr = ["+", "-", "/", "*"];
+var valid_operands;
+(function (valid_operands) {
+    valid_operands[valid_operands["+"] = 0] = "+";
+    valid_operands[valid_operands["-"] = 1] = "-";
+    valid_operands[valid_operands["/"] = 2] = "/";
+    valid_operands[valid_operands["*"] = 3] = "*";
+})(valid_operands || (valid_operands = {}));
+;
+let test = [1];
+console.log(test.includes(1));
+document.querySelector('.calc').addEventListener('click', (evt) => {
+    let button = evt.target;
     if (button.className.indexOf('num') !== -1) {
         console.log(button.innerHTML);
         output.value += button.innerHTML;
@@ -18,7 +29,7 @@ document.querySelector('.calc').addEventListener('click', function (evt) {
     }
     if (button.className.indexOf('equal') !== -1) {
         console.log(button.innerHTML);
-        var calculation = calculate( /*pass in params here*/);
+        let calculation = calculate( /*pass in params here*/);
         output.value = calculation.toString();
     }
     /* create another condition to clear the value of the input when C is pressed*/
@@ -28,5 +39,12 @@ function calculate( /*You fill in here*/) {
     /* based on the operand, find the correct value of the 2 numbers.  i.e., 4+5=9, 4*5=20, etc
     return that value
     */
+    let input = output.value;
+    let operands_regex = /\+|-|\/|\*/;
+    console.log(input);
+    console.log(input.split(operands_regex));
+    let operands = input.split("").filter(item => operands_arr.includes(item)).map(item => item);
+    let numbers = input.split(operands_regex).map(item => parseInt(item));
+    console.log(operands, numbers);
     return 0;
 }
--- calc.ts
+++ calc.ts
@@ -6,6 +6,15 @@ let output = document.querySelector('.output') as HTMLInputElement;
     
     Figure out how to set the values of num1 and num2 as the buttons are clicked
     */
+    let operands_arr: string[] = ["+" , "-" , "/" , "*" ];
+    // let operands_regex: RegExp = new RegExp(operands_arr.join("|"));
+    type operands_type = "+" | "-" | "/" | "*" ;
+    enum valid_operands {"+" , "-" , "/" , "*"};
+
+    let test = [1];
+
+    console.log(test.includes(1));
+
 document.querySelector('.calc').addEventListener('click',(evt)=>{
     let button = evt.target as HTMLButtonElement;
   
@@ -30,6 +39,16 @@ function calculate(/*You fill in here*/){
     /* based on the operand, find the correct value of the 2 numbers.  i.e., 4+5=9, 4*5=20, etc
     return that value
     */
-   return 0;
+
+    let input: string = output.value;
+    let operands_regex: RegExp = /\+|-|\/|\*/;
+    console.log(input);
+    console.log(input.split(operands_regex));
+
+    let operands: operands_type[] = input.split("").filter(item => operands_arr.includes(item)).map(item => item as operands_type);
+    let numbers: Number[] = input.split(operands_regex).map(item => new Number(item));
+    console.log(operands, numbers)
+
+    return 0;
 
 }
